5c8b832d1e00b7a7d910f250fcb65e0a1dfa165f
Kiran <kirankumardn74@gmail.com>
2025-12-15 23:01:24 +0530

    basic functionalities

---
+++ functions.py
@@ -0,0 +1,86 @@
+
+#Functions
+# def greet():
+#     print("Hello, sir welcome to learnbay!!")
+# greet()
+
+# def reception(members):
+#     if members <= 2:
+#         print("Provide a couple table")
+#     elif members <= 4:
+#         print("Provie a family table")
+#     elif members <= 10:
+#         print("Provide a party table")
+#     else:
+#         print("Provide a party hall")
+# reception(2)
+# reception(5)
+# reception(8)
+# reception(15)
+
+# def waiter(requirements):
+#     if requirements== "water":
+#         return "A bislery water bottle"
+#     if requirements == "Menu":
+#         return "Starts, Main course and drinks Menu"
+#     if requirements == "tissue":
+#         return "Worm water with lime"
+#     else: 
+#         return "Sry Sir/Madam"
+
+# response = waiter("tissue")
+# waiter("Menu")
+# # print(response)
+
+# def greet(name="chandu"):
+#     print("Hi My name is "+ name)
+# greet("Kiran")
+
+# greet()
+# def addNum(a,b):
+#     print(a+b)
+# addNum(2,2)
+# addNum(5,2)
+# addNum(2,9)
+
+# def addNum(a,b):
+#     print(a)
+#     print(b)
+#     print(a+b)
+# addNum(6,5)   
+
+# def addNumbers(*nums):
+#     currentSum=0
+#     for num in nums:
+#         currentSum +=num
+#     print(currentSum)
+# addNumbers(1,2,3,4,5)
+
+# def even_odd(num):
+#     if num%2==0:
+#         print("the number is even")
+#     else:
+#         print("the given number is odd")
+
+# even_odd(5)
+
+# lst=[1,2,3,4,5,6,7]
+# print("Entering to the code")
+# def evenoddSum(lst):
+#     even_sum=0
+#     odd_sum=0
+#     for i in lst:
+#         if i%2==0:
+#             even_sum=even_sum+i
+#         else:
+#             odd_sum=odd_sum+i
+#     return even_sum,odd_sum
+# print("entering to end func")
+# print(evenoddSum(lst))
+
+# def addition(a,b):
+#     return a+b
+
+# print(addition(2,2))
+
+#lambda function
